b321b4be7ee3e95a7f1387da33e8b0782158273e
rashad <152299230+cookierash@users.noreply.github.com>
2026-05-27 09:56:32 +0400

    favicon accessbility improvements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,14 @@ export const metadata: Metadata = {
   alternates: {
     canonical: "/",
   },
+  icons: {
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/favicon-32x32.png", type: "image/png", sizes: "32x32" },
+      { url: "/favicon-16x16.png", type: "image/png", sizes: "16x16" },
+    ],
+    apple: [{ url: "/apple-touch-icon.png", type: "image/png", sizes: "180x180" }],
+  },
   openGraph: {
     title: SITE_NAME,
     description: SITE_DESCRIPTION,
